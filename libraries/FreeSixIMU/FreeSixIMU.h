81b37cfe3dff930416044b3f0e9d18e8d57b191e
Carlos Montalvo <cmontalvo251@gmail.com>
2016-02-22 15:35:28 -0600

    Removed redundant code and update FreeSix gains

--- libraries/FreeSixIMU/FreeSixIMU.h
+++ libraries/FreeSixIMU/FreeSixIMU.h
@@ -38,8 +38,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // HMC5843 address is fixed so don't bother to define it
 
 
-#define twoKpDef  (2.0f * 0.5f) // 2 * proportional gain
-#define twoKiDef  (2.0f * 0.1f) // 2 * integral gain
+//#define twoKpDef  (2.0f * 0.5f) // 2 * proportional gain
+#define twoKpDef  (0.0f * 0.0f) // 2 * proportional gain
+//#define twoKiDef  (2.0f * 0.1f) // 2 * integral gain
+#define twoKiDef  (0.0f * 0.0f) // 2 * integral gain
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
