6a498648c67f3b9444b7f384afddb609ca8c4c25
Carlos Montalvo <cmontalvo251@gmail.com>
2016-05-21 22:02:06 -0500

    Updated the Serial_Example code

--- Serial_Example/Serial_Example.ino
+++ Serial_Example/Serial_Example.ino
@@ -1,20 +1,33 @@
+#include "CommunicationUtils.h"
+
+int numVars = 6; //Make sure this is the same as processing code
+float data[6]; //You must hardcode the number of variables.
+
 void setup() 
 {
   //initialize serial communications at a 9600 baud rate
   Serial.begin(9600);
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  for (int idx = 0;idx<numVars;idx++){
+    data[idx] = 0;
+  }
 }
 
 
 void loop()
 {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //send 'Hello, world!' over the serial port
-  Serial.println(random(1,10));
-  //wait 100 milliseconds so we don't drive ourselves crazy
-  delay(100);
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);
+  //digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  //Generate Data
+  for (int idx = 0;idx<numVars;idx++) {
+    data[idx]=sin((1.0/numVars)*3.141592654*(idx+1)*millis()/1000.0);
+  }
+  
+  //Write Data to Serial
+  serialPrintFloatArr(data,numVars);
+  Serial.println("\r\n"); //line break. Tells processing to stop reading data
+
+  //wait a bit so you don't write super freaking fast
+  //delay(10);
+  //digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
 }
 
