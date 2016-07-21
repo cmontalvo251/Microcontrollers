302eb60def4b45d7d7fa5262213f9fb6d42379d4
Carlos Montalvo <cmontalvo251@gmail.com>
2016-07-21 17:52:43 -0500

    Added 2 libraries and added functionality to pitot demo to have any number of sensors.

--- Demos/Pitot_Demo/Pitot_Demo.ino
+++ Demos/Pitot_Demo/Pitot_Demo.ino
@@ -2,11 +2,14 @@
 #include "CommunicationUtils.h"
 
 int numVars = 6; //Make sure this is the same as processing code
+int numPitot = 3;
 float data[6]; //You must hardcode the number of variables.
-float cal_voltage=0;
+float cal_voltage[3];
 float sensorvalue;
-float airspeed_then = 0;
-const int analogInPin = 3; //Airspeed sensor is hooked up to pin 3
+float airspeed_then[3];
+int analogInPin[3]; //Airspeed sensor is hooked up to pin 3
+float airspeed[3];
+float airspeed_now[3];
 
 void setup() 
 {
@@ -19,63 +22,74 @@ void setup()
   //Serial.print(cal_voltage);
   //Serial.print("\n");
   digitalWrite(13,LOW);
+
+  for (int idx = 0;idx<numPitot;idx++) {
+    analogInPin[idx] = idx+1;
+  }
+  digitalWrite(13,LOW);
+  //Read pitot sensor N times to calibrate sensor
+  for (int idx = 0;idx<numPitot;idx++) {
+      cal_voltage[idx] = 0;
+  }
+  int N = 10;
+  for (int idx = 0;idx<N;idx++) {
+    for (int jdx = 0;jdx<numPitot;jdx++){
+      sensorvalue = analogRead(analogInPin[jdx]);
+      cal_voltage[jdx]+=sensorvalue*(5.0/1023.0);
+      }
+      //delay(10);
+  }
+  for (int idx = 0;idx<numPitot;idx++) {
+    cal_voltage[idx] = cal_voltage[idx]/float(N);
+  }
 }
 
 
 void loop()
 {
-  if (cal_voltage == 0) {
-    digitalWrite(13,LOW);
-    //Read pitot sensor N times to calibrate sensor
-    cal_voltage = 0;
-    int N = 10;
-    for (int idx = 0;idx<N;idx++) {
-      sensorvalue = analogRead(analogInPin);
-      cal_voltage+=sensorvalue*(5.0/1023.0);
-      //delay(10);
-    }
-    cal_voltage = cal_voltage/float(N);
-  }
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
 
   //Pull Data from Airspeed sensor
-  float sensorvalue = analogRead(analogInPin);
-  //Convert to voltage
-  float raw_voltage = sensorvalue*(5.0/1023.0);
-  //Calibrate voltage
-  float voltage = raw_voltage - cal_voltage;
-  //Convert to pressure
-  float pressure = voltage/101.325;
-  //Compute Airspeed using bernoulli
-  float k = pow(pressure+1.0,2.0/7.0);
-  //Check for error
-  if (k < 1.0) {
-    k = 1.0;
+  for (int idx = 0;idx<numPitot;idx++) {
+    float sensorvalue = analogRead(analogInPin[idx]);
+    //Convert to voltage
+    float raw_voltage = sensorvalue*(5.0/1023.0);
+    //Calibrate voltage
+    float voltage = raw_voltage - cal_voltage[idx];
+    //Convert to pressure
+    float pressure = voltage/101.325;
+    //Compute Airspeed using bernoulli
+    float k = pow(pressure+1.0,2.0/7.0);
+    //Check for error
+    if (k < 1.0) {
+      k = 1.0;
+    }
+    //Compute speed of sound
+    //Based on temperature in Celsius
+    float tempC = 20.0;
+    float tempK = 273.15 + tempC;
+    float a_inf = sqrt(1.4*286.0*tempK);
+    //Compute Airspeed
+    airspeed_now[idx] = a_inf*(sqrt(5.0*(k-1.0)));
+    //Use a complimentary filter to filter out noise
+    float sigma = 0.03;
+    airspeed[idx] = (1.0-sigma)*airspeed_then[idx] + sigma*airspeed_now[idx];
+    airspeed_then[idx] = airspeed[idx];
+    data[idx] = airspeed[idx];
+//    Serial.print("Raw Bits = ");
+//    Serial.print(sensorvalue);
+//    Serial.print(" Voltage = ");
+//    Serial.print(voltage);
+//    Serial.print(" Airspeed = ");
+//    Serial.print(airspeed[idx]);
+//    Serial.print("\n");
   }
-  //Compute speed of sound
-  //Based on temperature in Celsius
-  float tempC = 20.0;
-  float tempK = 273.15 + tempC;
-  float a_inf = sqrt(1.4*286.0*tempK);
-  //Compute Airspeed
-  float airspeed_now = a_inf*(sqrt(5.0*(k-1.0)));
-  //Use a complimentary filter to filter out noise
-  float sigma = 0.03;
-  float airspeed = (1.0-sigma)*airspeed_then + sigma*airspeed_now;
-  airspeed_then = airspeed;
-  data[0] = airspeed;
   //Random Number
-  for (int idx = 1;idx<6;idx++){
-    data[idx] = random(1,10);
+  for (int idx = numPitot;idx<6;idx++){
+     data[idx] = random(1,10);
   }
   
-//  Serial.print("Raw Bits = ");
-//  Serial.print(sensorvalue);
-//  Serial.print(" Voltage = ");
-//  Serial.print(voltage);
-//  Serial.print(" Airspeed = ");
-//  Serial.print(airspeed);
-//  Serial.print("\n");
+
   
   //Write Data to Serial
   serialPrintFloatArr(data,numVars);
