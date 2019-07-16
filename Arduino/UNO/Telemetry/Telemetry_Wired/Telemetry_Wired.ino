c1c93d86dcea43b7add0b34f797a0177c4beeb1e
Carlos Montalvo <cmontalvo251@gmail.com>
2019-07-15 22:05:54 -0500

    Added Telemetry with Software Serial

--- Arduino/UNO/Telemetry/Telemetry_Wired/Telemetry_Wired.ino
+++ Arduino/UNO/Telemetry/Telemetry_Wired/Telemetry_Wired.ino
@@ -1,5 +1,24 @@
-#include <Wire.h>
-#include "CommunicationUtils.h"
+void serialFloatPrint(float f) {
+  byte * b = (byte *) &f;
+  for(int i=0; i<4; i++) {
+    
+    byte b1 = (b[i] >> 4) & 0x0f;
+    byte b2 = (b[i] & 0x0f);
+    
+    char c1 = (b1 < 10) ? ('0' + b1) : 'A' + b1 - 10;
+    char c2 = (b2 < 10) ? ('0' + b2) : 'A' + b2 - 10;
+    
+    Serial.print(c1); //does this output a command between prints?
+    Serial.print(c2); //otherwise line 75 on .pde won't work because their aren't any commas (Look up like 10 lines of code)
+  }
+}
+
+void serialPrintFloatArr(float * arr, int length) {
+  for(int i=0; i<length; i++) {
+    serialFloatPrint(arr[i]);
+    Serial.print(","); //The commas are here dude. 
+  }
+}
 
 int numVars = 4;
 String receive_message;
