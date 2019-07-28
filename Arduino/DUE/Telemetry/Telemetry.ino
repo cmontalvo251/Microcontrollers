da832a5cd843a877347765451640e92b0910a441
Carlos Montalvo <cmontalvo251@gmail.com>
2019-07-27 22:15:15 -0500

    Added some more telemetry scripts for DUE

--- Arduino/DUE/Telemetry/Telemetry.ino
+++ Arduino/DUE/Telemetry/Telemetry.ino
@@ -1,12 +1,10 @@
 #include "telemetry.h"
 
-//This kicks off the constructor and sends it Serial2
-Telemetry Xbee(&Serial2);
+Telemetry Xbee;
 
-//Nothing to do here for Telemetry since constructor class does that for us
 void setup()
 {
-  
+  Xbee.begin(&Serial2);
 }
 
 //Just grab time_now and send to Xbee. Obviously need to send it data as well.
@@ -14,10 +12,10 @@ void loop()
 {
   //Take Data
   float time_now = millis()/1000.0;
+  
   float phi = 0.1*time_now;
   float theta = -0.2*time_now;
   float psi = 0.5*time_now;
-
   //Load into out vector
   Xbee.serialoutarray[0] = time_now;
   Xbee.serialoutarray[1] = phi;
---
+++ Arduino/DUE/Telemetry_9DOF/Telemetry_9DOF.ino
@@ -0,0 +1,49 @@
+#include "telemetry.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+Telemetry Xbee;
+
+Adafruit_BNO055 bno;
+
+void setup()
+{
+  ///Setup telemetry
+  Xbee.begin(&Serial2);
+
+  ///Setup the 9DOF
+  bno = Adafruit_BNO055(55);
+
+  Serial.begin(9600); //For eerror codes.
+
+  // IMU //
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  bno.setExtCrystalUse(true);
+}
+
+//Just grab time_now and send to Xbee. Obviously need to send it data as well.
+void loop()
+{
+  //Take Data
+  float time_now = millis()/1000.0;
+
+  //Get roll pitch yaw
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  float phi = PI/180.0*(euler.y()); 
+  float theta = PI/180.0*(euler.z()-9); //The 9.0 is because the sensor is not mounted flat on the PCB
+  float psi = PI/180.0*euler.x();
+  
+  //Load into out vector
+  Xbee.serialoutarray[0] = time_now;
+  Xbee.serialoutarray[1] = phi;
+  Xbee.serialoutarray[2] = theta;
+  Xbee.serialoutarray[3] = psi; 
+  
+  Xbee.write(time_now);  
+}
