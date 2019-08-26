d6f0ee4577e162393f54d98518a2f8dd83c1056f
Carlos Montalvo <cmontalvo251@gmail.com>
2019-08-26 11:19:24 -0500

    Added some new codes to CPX

--- Circuit_Playground/CircuitPython/blink.py
+++ Circuit_Playground/CircuitPython/blink.py
@@ -1,10 +1,10 @@
 import board
 import digitalio
 import time
- 
+
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
- 
+
 while True:
     led.value = True
     time.sleep(0.5)
--- Circuit_Playground/CircuitPython/measure_theta.py
+++ Circuit_Playground/CircuitPython/measure_theta.py
@@ -16,21 +16,21 @@ def Compute_Theta(theta_in):
     x_total = x_total / 10
     y_total = y_total / 10
     z_total = z_total / 10
-    
+
     #From here we need to rotate to the robot frame
     angle = 47.*math.pi/180.
     cos = math.cos(angle)
     sin = math.sin(angle)
     x_robot = cos*x_total + sin*y_total
     y_robot = -sin*x_total + cos*y_total
-    
+
     #Then we can compute theta the pitch angle
     theta = math.atan(y_robot/z_total)*180./math.pi
-    
+
     elapsed_time = 0.1
-    
+
     thetadot = (theta_in - theta)/elapsed_time
-    
+
     #print((x_robot,y_robot,z_total))
     return [theta,thetadot]
 
@@ -41,3 +41,4 @@ while True:
     [theta,thetadot_new] = Compute_Theta(theta)
     thetadot = thetadot*s + thetadot_new*(1-s)
     print((theta,thetadot))
+    time.sleep(0.1)
