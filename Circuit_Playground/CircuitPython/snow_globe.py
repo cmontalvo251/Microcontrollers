7ce76308cb475e868415d3319b7b445378f6cc6f
Carlos Montalvo <cmontalvo251@gmail.com>
2019-11-18 16:24:17 -0600

    Added some new codes

--- Circuit_Playground/CircuitPython/snow_globe.py
+++ Circuit_Playground/CircuitPython/snow_globe.py
@@ -1,25 +1,24 @@
 # Snow Globe
 # for Adafruit Circuit Playground express
 # with CircuitPython
- 
 from adafruit_circuitplayground.express import cpx
 import math
 import time
- 
+
 ROLL_THRESHOLD = 30  # Total acceleration
 cpx.pixels.brightness = 0.1  # set brightness value
- 
+
 WHITE = (65, 65, 65)
 RED = (220, 0, 0)
 GREEN = (0, 220, 0)
 BLUE = (0, 0, 220)
 SKYBLUE = (0, 20, 200)
 BLACK = (0, 0, 0)
- 
+
 # Initialize the global states
 new_roll = False
 rolling = False
- 
+
 def fade_pixels(fade_color):  # pick from colors defined above, e.g., RED, GREEN, BLUE, WHITE, etc.
     # fade up
     for j in range(25):
@@ -27,21 +26,21 @@ def fade_pixels(fade_color):  # pick from colors defined above, e.g., RED, GREEN
         cpx.pixels.brightness = pixel_brightness
         for i in range(10):
             cpx.pixels[i] = fade_color
- 
+
     # fade down
     for k in range(25):
         pixel_brightness = (0.25 - (k * 0.01))
         cpx.pixels.brightness = pixel_brightness
         for i in range(10):
             cpx.pixels[i] = fade_color
- 
+
 # fade in the pixels
 fade_pixels(GREEN)
- 
+
 def play_song(song_number):
     # 1: Jingle bells
     # 2: Let It Snow
- 
+
     # set up time signature
     whole_note = 1.5  # adjust this to change tempo of everything
     # these notes are fractions of the whole note
@@ -49,7 +48,7 @@ def play_song(song_number):
     quarter_note = whole_note / 4
     dotted_quarter_note = quarter_note * 1.5
     eighth_note = whole_note / 8
- 
+
     # set up note values
     A3 = 220
     Bb3 = 233
@@ -66,36 +65,36 @@ def play_song(song_number):
     A4 = 440
     Bb4 = 466
     B4 = 494
- 
+
     if song_number == 1:
         # jingle bells
         jingle_bells_song = [[E4, quarter_note], [E4, quarter_note],
         [E4, half_note], [E4, quarter_note], [E4, quarter_note],
         [E4, half_note], [E4, quarter_note], [G4, quarter_note],
         [C4, dotted_quarter_note], [D4, eighth_note], [E4, whole_note]]
- 
+
         for n in range(len(jingle_bells_song)):
             cpx.start_tone(jingle_bells_song[n][0])
             time.sleep(jingle_bells_song[n][1])
             cpx.stop_tone()
- 
- 
+
+
     if song_number == 2:
         # Let It Snow
         let_it_snow_song = [[B4, dotted_quarter_note], [A4, eighth_note],
         [G4, quarter_note], [G4, dotted_quarter_note], [F4, eighth_note],
         [E4, quarter_note], [E4, dotted_quarter_note], [D4, eighth_note],
         [C4, whole_note]]
- 
+
         for n in range(len(let_it_snow_song)):
             cpx.start_tone(let_it_snow_song[n][0])
             time.sleep(let_it_snow_song[n][1])
             cpx.stop_tone()
- 
+
 play_song(1)  # play music on start
 
 #Initialize song_number to 1
-song_number = 1 
+song_number = 1
 
 # Loop forever
 while True:
@@ -113,29 +112,29 @@ while True:
     x_total = x_total / 10
     y_total = y_total / 10
     z_total = z_total / 10
- 
+
     total_accel = math.sqrt(x_total*x_total + y_total*y_total + z_total*z_total)
- 
+
     # Check for rolling
     if total_accel > ROLL_THRESHOLD:
         roll_start_time = time.monotonic()
         new_roll = True
         rolling = True
         #print('shaken')
- 
+
     # Rolling momentum
     # Keep rolling for a period of time even after shaking stops
     if new_roll:
         if time.monotonic() - roll_start_time > 2:  # seconds to run
                 rolling = False
- 
+
     # Light show
     if rolling:
         fade_pixels(SKYBLUE)
         fade_pixels(WHITE)
         cpx.pixels.brightness = 0.8
         cpx.pixels.fill(WHITE)
- 
+
     elif new_roll:
         new_roll = False
         # play a song!
