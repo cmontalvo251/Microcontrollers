79c08d1af4c7313a531ae5a5fab3133dedebfc87
Carlos Montalvo <cmontalvo251@gmail.com>
2019-12-17 20:20:30 -0600

    Finished the airplane demo

--- Circuit_Playground/CircuitPython/Airplane_Demo/airplane_demo.py
+++ Circuit_Playground/CircuitPython/Airplane_Demo/airplane_demo.py
@@ -2,10 +2,15 @@ import time
 import board
 import busio
 import digitalio
-import adafruit_lis3dh
+import adafruit_lis3dh ##On board accelerometer
 import math
 import pulseio
 import neopixel
+import adafruit_mpu6050 ##off board MPU6050
+
+##Initialize MPU6050
+i2c_ = busio.I2C(board.SCL, board.SDA)
+mpu = adafruit_mpu6050.MPU6050(i2c_)
 
 # Initialize PWM output for the servo (on pin A2):
 servo = pulseio.PWMOut(board.A2, frequency=50)
@@ -17,7 +22,20 @@ lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=_int1)
 lis3dh.range = adafruit_lis3dh.RANGE_8_G
 
 #Initialize Neopixels
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=10,auto_write=True)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0,auto_write=True)
+
+N = 100
+gz_a = 0
+ax_a = 0
+print('Calibrating...')
+print('DO NOT MOVE!!!')
+for i in range(0,N):
+    ax,ay,az = lis3dh.acceleration
+    ax_a += ax
+    gx,gy,gz = mpu.gyro
+    gz_a += gz
+gz_a /= N
+ax_a /= N
 
 # Create a function to simplify setting PWM duty cycle for the servo:
 def servo_duty_cycle(pulse_ms, frequency=50):
@@ -28,20 +46,29 @@ def servo_duty_cycle(pulse_ms, frequency=50):
 while True:
     #Accelerometer - H portion of Control Block Diagram
     x,y,z = lis3dh.acceleration
+    #xm,ym,zm = mpu.acceleration
+    #print((xm,ym,zm,x,y,z))
     #If the USB port is pointed along the y-body axis
     #z is down and x is forward
-    theta_deg = -math.atan(x/z)*180./3.14
+    theta_deg = -math.atan((x-ax_a)/z)*180./3.14
+    #Also read angular velocity
+    gx,gy,gz = mpu.gyro
+    #print((gx,gy,gz))
+    theta_dot = gz - gz_a #this is in degrees per second
 
     #Command - This is the reference command
     theta_command = 0*3.14/180.
+    theta_dot_command = 0.0
 
     #Summing block
     error = theta_command - theta_deg
+    error_dot = theta_dot_command - theta_dot
 
     #Control Block - C
     #Proportional gain
-    kp = -0.5 #start small!!!!
-    elevator_command = kp*error
+    kp = 0*-0.5 #start small!!!!
+    kd = 0*-0.2
+    elevator_command = kp*error + kd*error_dot
 
     #we need to make sure we dont break anything
     #so we need a saturation block
@@ -96,5 +123,5 @@ while True:
         pixels[i] = (rl,rl,rl)
 
     #Print everything
-    print((theta_deg,error,elevator_command,duty,fl,rl))
+    print((theta_deg,theta_dot,error,elevator_command,duty,fl,rl))
     time.sleep(0.1)
