77d4ff4026da1e2a3bcbd928fa5d863e3ce38f92
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-04-26 12:17:58 -0500

    Added a touch screen input to change the time on the fly

--- PyPortal/temp_light_plotter.py
+++ PyPortal/temp_light_plotter.py
@@ -3,20 +3,41 @@ import displayio
 import time
 import busio
 import adafruit_adt7410
-from digitalio import DigitalInOut
+from digitalio import DigitalInOut, Direction, Pull
 from analogio import AnalogIn
 import terminalio
 from adafruit_display_text import label
+import adafruit_touchscreen
 
 display = board.DISPLAY
 
 # Create a bitmap with three colors
 bitmap = displayio.Bitmap(display.width, display.height, 4)
-
+time_sleep = 0
+time_next = 0
+time_range_hrs = 2
 def clear():
     for x in range(0,display.width):
         for y in range(0,display.height):
             bitmap[x,y] = 0
+
+def createText(level,maxval,minval,x,incolor):
+    val = int(level*(maxval-minval)+minval)
+    textobj = label.Label(terminalio.FONT,text=str(val),color=incolor)
+    textobj.x = int(x*display.width)
+    textobj.y = int((1-level)*display.height)
+    return textobj
+
+def change_time(time_range_hrs):
+    global time_sleep
+    #time_range_hrs = 8.0
+    #time_next = 0
+    time_sleep = time_range_hrs*60*60/display.width
+    time_range_text = label.Label(terminalio.FONT,text="R = " + str(time_range_hrs)+" h "+"S = "+str(int(time_sleep))+" s",color=WHITE)
+    time_range_text.x = int(0.1*display.width)
+    time_range_text.y = int(0.1*display.height)
+    print("Sleep Range = ",time_sleep)
+    return time_range_text
 
 BLACK = 0x000000
 WHITE = 0xffffff #Light color
@@ -50,13 +71,6 @@ tempmin = 50.
 #tempmin = 25.
 tempslope = display.height/(tempmin-tempmax)
 
-def createText(level,maxval,minval,x,incolor):
-    val = int(level*(maxval-minval)+minval)
-    textobj = label.Label(terminalio.FONT,text=str(val),color=incolor)
-    textobj.x = int(x*display.width)
-    textobj.y = int((1-level)*display.height)
-    return textobj
-
 #Text
 tempx = 0.8
 temp_texts = []
@@ -73,14 +87,8 @@ for i in range(1,10,4):
     group.append(light_texts[-1])
 
 ###Make a grid now
-time_range_hrs = 8.0
-time_next = 0
-time_sleep = time_range_hrs*60*60/display.width
-time_range_text = label.Label(terminalio.FONT,text="R = " + str(time_range_hrs)+" h "+"S = "+str(int(time_sleep))+" s",color=WHITE)
-time_range_text.x = int(0.1*display.width)
-time_range_text.y = int(0.1*display.height)
+time_range_text = change_time(time_range_hrs)
 group.append(time_range_text)
-print("Sleep Range = ",time_sleep)
 
 #Setup light and temperature
 i2c_bus = busio.I2C(board.SCL, board.SDA)
@@ -92,17 +100,40 @@ adc = AnalogIn(board.LIGHT)
 current_vals = label.Label(terminalio.FONT,text="L: "+str(adc.value)+" T: "+str(adt.temperature),color=WHITE)
 current_vals.x = int(0.1*display.width)
 current_vals.y = int(0.2*display.height)
-group.append(current_vals)
 # Add the Group to the Display
+group.append(current_vals)
+
+##THen finally show the group
 display.show(group)
 
 #Line Height
 line_height = 5
 
+#Create a button for the touch screen
+ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR,board.TOUCH_YD, board.TOUCH_YU)
+
 ctr = display.width-1
 yball = 0
 time_start = time.monotonic()
 while 1:
+    p = ts.touch_point
+    if p:
+        print('Screen touched')
+        time_range_hrs += 2
+        if time_range_hrs > 24:
+            time_range_hrs = 2
+        current_vals = group[-1]
+        #pop the current vals
+        group.pop()
+        #pop the current labels
+        group.pop()
+        #Create a new label
+        time_range_text = change_time(time_range_hrs)
+        #Append the label
+        group.append(time_range_text)
+        #Append the current_vals
+        group.append(current_vals)
+        time.sleep(1)
     if time_next < (time.monotonic()-time_start):
         print(time_next,time.monotonic()-time_start)
         time_next += time_sleep
