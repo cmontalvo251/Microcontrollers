8f0a1640b6c269adbb75efd96b69226d1f7871ff
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-07-03 10:35:13 -0500

    added a button press datalogger

---
+++ Circuit_Playground/CircuitPython/Data_Logging/button_presses.py
@@ -0,0 +1,11 @@
+import board
+import digitalio
+import time
+
+buttonA = digitalio.DigitalInOut(board.D4)
+buttonA.direction = digitalio.Direction.INPUT
+buttonA.pull = digitalio.Pull.DOWN
+
+while True:
+    print(int(buttonA.value))
+    time.sleep(0.1)
