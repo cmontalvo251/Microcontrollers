0268d165408669ba54bd7f24445791e2a1a9aedd
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-09-16 22:01:56 -0500

    I can now send and receive 3 numbers via HEX/Serial

---
+++ Arduino/UNO/Serial/Send3_Receive3/Send3_Receive3.ino
@@ -0,0 +1,60 @@
+extern void SendReceiveNumber(void);
+
+void blinktwice(int d) {
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(d);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(d);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(d);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(d);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  //Turn on the Builtin LED
+  pinMode(LED_BUILTIN,OUTPUT);
+  //Blink once to notify that the arduino is ready
+  //blinktwice(1000);
+  blinktwice(100);
+}
+
+void loop() {  
+  char c;
+
+  //Ok so this code here basically waits until the arduino
+  //read a 'w'. 
+  do {
+    c=Serial.read();
+  } while (c != 'w');
+  //In order to notify the user that the arduino received a w
+  //and a new line below.
+  digitalWrite(LED_BUILTIN,HIGH);
+
+  //Serial.print('q');
+  //Set the first Neopixel to red
+  //CircuitPlayground.setPixelColor(0,255,0,0);
+  do {
+    c=Serial.read();
+  } while (c != '\r');
+
+  //Serial.print('r');
+  //Then set the second Neopixel to red
+  //CircuitPlayground.setPixelColor(1,255,0,0);
+
+  //Once that happens the Arduino is going to return 'w\r\n'
+  //to whatever is listening
+  //This also returns w\r\n to the arduino
+  c = 'w'; //ASCII CODE = 119
+  Serial.print(c);
+  c = '\r'; //ASCII CODE = 13
+  Serial.print(c);
+  c = '\n'; //ASCII CODE = 10
+  Serial.print(c); 
+  
+  //After that it will run the DoHIL routine
+  SendReceiveNumber();
+
+}
---
+++ Arduino/UNO/Serial/Send3_Receive3/SendReceive.cpp
@@ -0,0 +1,109 @@
+/*
+ * DoHIL.c
+ *
+ *  Created on: Aug 17, 2020
+ *      Author: Sam Russ
+ *      Edited to just send 1 number: Carlos Montalvo
+ */
+ 
+#include "HardwareSerial.h"
+#include "Arduino.h"
+
+//This is needed to convert from floats to longs
+union inparser {
+  long inversion;
+  float floatversion;
+} ;
+
+// Declared these global so they can be "spied on" during debug
+#define MAXLINE	60
+char FirstLine[MAXLINE];
+
+void SendReceiveNumber(){
+
+	char inchar;
+	int i = 0;
+
+	union inparser inputvar;
+
+	do {
+    do{
+      inchar = Serial.read();
+    } while (inchar == -1);
+
+		FirstLine[i++] = inchar;
+		//PRINTF("%c",inchar);
+	} while ((inchar != '\r') && (i<MAXLINE));
+  FirstLine[i] = '\0'; //Null terminate the string
+
+	// Format from PC: (It's looking for a \r right?)
+	// H:nnnnnnnn H:nnnnnnnn H:nnnnnnnn \r 
+
+  //Echo back what you received (just for debugging)
+  //Serial.print(FirstLine);
+
+
+  /////////////////?FIRST NUMBER/////////////////////
+  //Convert Hex Digits to an integer
+	inputvar.inversion = 0;
+	for(i=2;i<10;i++){
+		inputvar.inversion <<= 4;
+		inputvar.inversion |= (FirstLine[i] <= '9' ? FirstLine[i] - '0' : toupper(FirstLine[i]) - 'A' + 10);
+	}
+
+  //Convert integer to float
+	float number = inputvar.floatversion;
+  //Multiply float by 2
+  float outnumber = 2*number;
+
+  //Convert out float to an integer
+  inputvar.floatversion = outnumber;
+  
+  char outline[20];
+  sprintf(outline, "H:%08lx \0",inputvar.inversion);
+  Serial.print(outline);
+  Serial.print('\r');
+  ////////////////////////////////////////////////////
+
+  ////////////////SECOND NUMBER/////////////////////
+  //Convert Hex Digits to an integer
+  inputvar.inversion = 0;
+  for(i=13;i<21;i++){
+    inputvar.inversion <<= 4;
+    inputvar.inversion |= (FirstLine[i] <= '9' ? FirstLine[i] - '0' : toupper(FirstLine[i]) - 'A' + 10);
+  }
+
+  //Convert integer to float
+  number = inputvar.floatversion;
+  //Multiply float by 2
+  outnumber = 2*number;
+
+  //Convert out float to an integer
+  inputvar.floatversion = outnumber;
+  
+  sprintf(outline, "H:%08lx \0",inputvar.inversion);
+  Serial.print(outline);
+  Serial.print('\r');
+
+  ////////////////THIRD NUMBER/////////////////////
+  //Convert Hex Digits to an integer
+  inputvar.inversion = 0;
+  for(i=24;i<32;i++){
+    inputvar.inversion <<= 4;
+    inputvar.inversion |= (FirstLine[i] <= '9' ? FirstLine[i] - '0' : toupper(FirstLine[i]) - 'A' + 10);
+  }
+
+  //Convert integer to float
+  number = inputvar.floatversion;
+  //Multiply float by 2
+  outnumber = 2*number;
+
+  //Convert out float to an integer
+  inputvar.floatversion = outnumber;
+  
+  sprintf(outline, "H:%08lx \0",inputvar.inversion);
+  Serial.print(outline);
+  Serial.print('\r');
+  ///////////////////////////////////////////////////
+
+}
