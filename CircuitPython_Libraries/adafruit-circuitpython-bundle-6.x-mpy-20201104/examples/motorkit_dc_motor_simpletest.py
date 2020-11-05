05b1eadf130a6da5c4e261448ffc548939217c58
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-11-04 22:43:52 -0600

    Added CircuitPython Libraries 6.x.x

---
+++ CircuitPython_Libraries/adafruit-circuitpython-bundle-6.x-mpy-20201104/examples/motorkit_dc_motor_simpletest.py
@@ -0,0 +1,10 @@
+"""Simple test for using adafruit_motorkit with a DC motor"""
+import time
+import board
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit(i2c=board.I2C())
+
+kit.motor1.throttle = 1.0
+time.sleep(0.5)
+kit.motor1.throttle = 0
---
+++ CircuitPython_Libraries/adafruit-circuitpython-bundle-6.x-mpy-20201104/examples/motorkit_stepper_simpletest.py
@@ -0,0 +1,10 @@
+"""Simple test for using adafruit_motorkit with a stepper motor"""
+import time
+import board
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit(i2c=board.I2C())
+
+for i in range(100):
+    kit.stepper1.onestep()
+    time.sleep(0.01)
