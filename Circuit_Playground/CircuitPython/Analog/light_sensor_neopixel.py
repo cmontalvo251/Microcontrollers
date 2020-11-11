86a9b4c3e9b984c65af6764d73ddb7cd708719d4
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-11-11 14:15:52 -0600

    Added a bunch of IR codes and light sensor codes

---
+++ Circuit_Playground/CircuitPython/Analog/light_sensor_neopixel.py
@@ -0,0 +1,21 @@
+import time
+import board
+from analogio import AnalogIn
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=1.0)
+
+analogin = AnalogIn(board.A8)
+
+def getVoltage(pin):  # helper
+    return (pin.value * 3.3) / 65536
+
+while True:
+    #print("Analog Voltage: %f" % getVoltage(analogin))
+    val = getVoltage(analogin)
+    print((val,))
+    if val < 0.5:
+        pixels.fill((255,0,0))
+    else:
+        pixels.fill((0,0,0))
+    time.sleep(0.1)
--- Circuit_Playground/CircuitPython/IR/IR_ON_OFF.py
+++ Circuit_Playground/CircuitPython/IR/IR_ON_OFF.py
@@ -5,7 +5,7 @@ import adafruit_irremote
 import neopixel
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=1.0)
-
+ON = 0
 # Create a 'pulseio' input, to listen to infrared signals on the IR receiver
 pulsein = pulseio.PulseIn(board.IR_RX, maxlen=120, idle_state=True)
 # Create a decoder that will take pulses and turn them into numbers
@@ -25,12 +25,18 @@ while True:
         print("Failed to decode: ", e.args)
         continue
 
-    print(pulses)
-    #print("Infrared code received: ", received_code)
+    #print(pulses)
+    print("Infrared code received: ", received_code)
+
     #if received_code[2] == 247:
     #    print("Channel Down")
-    #    pixels.fill((255,0,0))
+    if ON:
+        pixels.fill((0,0,0))
+        ON = 0
+    else:
+        ON = 1
+        pixels.fill((255,0,0))
     #elif received_code[2] == 183:
     #    print("Channel Up")
     #    pixels.fill((0,255,0))
-    #pixels.show()
+    pixels.show()
