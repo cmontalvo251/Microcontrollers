a533aaee291618d2ac6dbc2cdd950c7ad144af37
Carlos Montalvo <cmontalvo@southalabama.edu>
2021-06-22 07:16:52 -0500

    Started adding a routine to log Sensor data on Wooden quad

---
+++ Arduino/DUE/Log_BNO_MPL/Log_BNO_MPL.ino
@@ -0,0 +1,15 @@
+////THIS ROUTINE WILL SIMPLY READ DATA FROM THE BNO 9DOF SENSOR AND MPL SENSOR
+////AND THEN PRINT THEM TO SERIAL
+
+#include <IMU.h>
+IMU orientation;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
