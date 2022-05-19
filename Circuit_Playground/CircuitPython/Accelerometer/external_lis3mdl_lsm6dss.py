ba58407bd4df4f1b124058036d3a6f48d33be578
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-05-19 10:49:18 -0500

    Fixed the external lis3mdl_lsm6dss code

--- Circuit_Playground/CircuitPython/Accelerometer/external_lis3mdl_lsm6dss.py
+++ Circuit_Playground/CircuitPython/Accelerometer/external_lis3mdl_lsm6dss.py
@@ -2,21 +2,19 @@ import time
 import board
 import busio
 import digitalio
-#import adafruit_lis3dh
-#from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+import adafruit_lis3dh
 import adafruit_lis3mdl
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
 
-i2c_ext = busio.I2C(board.SCL, board.SDA)
-
-#print(dir(adafruit_lsm6ds))
-#sensor_ext = LSM6DS33(i2c_ext,address=0x6a)
-mag = adafruit_lis3mdl.LIS3MDL(i2c_ext,address=0x1c)
+i2c = busio.I2C(board.SCL, board.SDA)
+accelgyro = LSM6DS33(i2c,address=0x6a)
+mag = adafruit_lis3mdl.LIS3MDL(i2c)
 
 ##Accelerometer is hooked up to SDA/SCL which is I2C
-#i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
-#_int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
-#lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=_int1)
-#lis3dh.range = adafruit_lis3dh.RANGE_8_G
+i2cCPB = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
+_int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
+lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2cCPB, address=0x19, int1=_int1)
+lis3dh.range = adafruit_lis3dh.RANGE_8_G
 
 #while not i2c_ext.try_lock():
 #    pass
@@ -32,14 +30,11 @@ mag = adafruit_lis3mdl.LIS3MDL(i2c_ext,address=0x1c)
 ## 0x1e and 0x6a on my broken one
 ## 0x1c and 0x6a on a working one - 6a is the accelerometer/gyro and the 1e/1c is the magneometer
 
+
 while True:
-    #x,y,z = lis3dh.acceleration
-    #xe,ye,ze = sensor_ext.acceleration
-    #gx,gy,gz = sensor_ext.gyro
+    axCPB,ayCPB,azCPB = lis3dh.acceleration
     bx,by,bz = mag.magnetic
-    print((bx,by,bz))
-    #print((x,xe,y,ye,z,ze))
-    #print((x,y,z))
-    #print((xe,ye,ze))
-    #print((gx,gy,gz))
+    gx,gy,gz = accelgyro.gyro
+    ax,ay,az = accelgyro.acceleration
+    print(axCPB,ayCPB,azCPB,ax,ay,az,gx,gy,gz,bx,by,bz)
     time.sleep(0.1)
