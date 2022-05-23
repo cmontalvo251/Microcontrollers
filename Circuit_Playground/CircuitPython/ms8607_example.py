bbb17ac0a4af37bce634ec0f8b4c43bc9b5e61b3
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-05-23 13:50:35 -0500

    Added ms8607 example and CPB datalogger for the ms8607

---
+++ Circuit_Playground/CircuitPython/ms8607_example.py
@@ -0,0 +1,13 @@
+from time import sleep
+import board
+from adafruit_ms8607 import MS8607
+
+i2c = board.I2C()
+sensor = MS8607(i2c)
+
+while True:
+
+    print("Pressure: %.2f hPa" % sensor.pressure)
+    print("Temperature: %.2f C" % sensor.temperature)
+    print("Humidity: %.2f %% rH" % sensor.relative_humidity)
+    sleep(1)
