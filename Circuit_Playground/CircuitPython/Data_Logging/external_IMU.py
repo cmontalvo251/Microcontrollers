90cc24c3fe146996eb0e79b936c88bcb11ff2210
Carlos Montalvo MSI Prestige <cmontalvo251@gmail.com>
2023-03-02 11:42:36 -0600

    Added a new external IMU datalogging code

---
+++ Circuit_Playground/CircuitPython/Data_Logging/external_IMU.py
@@ -0,0 +1,81 @@
+import time
+import board
+import busio
+import digitalio
+import adafruit_lis3dh
+import adafruit_lis3mdl
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+import neopixel
+
+i2c = busio.I2C(board.SCL, board.SDA)
+accelgyro = LSM6DS33(i2c,address=0x6a)
+
+#Set up pixels
+pixel_brightness = 0.25
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=pixel_brightness)
+
+# switch = digitalio.DigitalInOut(board.D5)  # For Feather M0/M4 Express
+##Button Presses
+print(dir(board))
+switch = digitalio.DigitalInOut(board.D7)  # For Circuit Playground Express
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
+
+##Accelerometer is hooked up to SDA/SCL which is I2C
+#i2cCPB = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
+#_int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
+#lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2cCPB, address=0x19, int1=_int1)
+#lis3dh.range = adafruit_lis3dh.RANGE_8_G
+
+if switch.value == True:
+    print('Not taking data')
+    pixels.fill((255,0,0))
+    DATA = False
+else:
+    print('Taking data....')
+    pixels.fill((0,255,0))
+    DATA = True
+    file = open('Disc_Data.txt','w')
+#while not i2c.try_lock():
+#    pass
+
+#try:
+#    while True:
+#        print("I2C addresses found:", [hex(device_address) for device_address in i2c.scan()])
+#        time.sleep(2)
+
+#finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
+#    i2c.unlock()
+## 0x1e and 0x6a on my broken one
+## 0x1c and 0x6a on a working one - 6a is the accelerometer/gyro and the 1e/1c is the magneometer
+prevtime = time.monotonic()
+val = 255
+while True:
+    t = time.monotonic()
+    gx,gy,gz = accelgyro.gyro
+    ax,ay,az = accelgyro.acceleration
+    #print(t,ax,ay,az,gx,gy,gz)
+    print((t,t-prevtime,ax,ay,az,gx,gy,gz,switch.value))
+    val -= 10
+    if val < 0:
+        val = 255
+    if DATA:
+        print('Taking data')
+        pixels.fill((0,val,0))
+        output = str(t) + " " + str(ax) + " " + str(ay) + " " + str(az) + " " + str(gx) + " " + str(gy) + " " + str(gz) + str('\n')
+        file.write(output)
+        file.flush()
+        if switch.value == True:
+            DATA = 0
+            file.close()
+    else:
+        print('Not taking data. Flip the switch and hit reset...')
+        pixels.fill((val,0,0))
+        if switch.value == False:
+            print('Intentionally writing bad code to throw an error....')
+            file.write('')
+    #print((gx,gy,gz))
+    prevtime = t
+    time.sleep(0.02)
+
+# Write your code here :-)
