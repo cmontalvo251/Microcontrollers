09ff976f2ee042b9182fb5a732978225561d151a
Carlos Montalvo <cmontalvo@southalabama.edu>
2023-06-16 14:29:30 -0500

    Added a bunch of extra user information to the simple method 3 code referenced by the textbook

--- Circuit_Playground/CircuitPython/Data_Logging/write_button_presses_disk.py
+++ Circuit_Playground/CircuitPython/Data_Logging/write_button_presses_disk.py
@@ -1,6 +1,7 @@
 import time
 import board
 import digitalio
+import neopixel
 
 switch = digitalio.DigitalInOut(board.D7)
 switch.direction = digitalio.Direction.INPUT
@@ -14,15 +15,30 @@ led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 led.value = True
 
+##MAKE A COLOR WHEEL
+colors = [(255,0,0),(0,255,0),(0,0,255)]
+c = 0
+
+#SETUP PIXELS
+pixel_brightness = 0.25
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=pixel_brightness)
+
 if switch.value == False:
     file = open('Method3.txt','w')
+    FILEOPEN = True
 else:
     print('Not opening file for writing')
+    FILEOPEN = False
 
 while True:
-    led.value = True
+    led.value = not led.value
     print(time.monotonic(),int(buttonA.value))
     if switch.value == False:
+        ##CHANGE COLOR EVERY TIME YOU TAKE DATA
+        pixels[0] = colors[c]
+        c+=1
+        if c >= len(colors):
+            c = 0
         print('Writing Data to Disk')
         output = str(time.monotonic()) + " " + str(int(buttonA.value)) + str('\n')
         file.write(output)
@@ -30,4 +46,9 @@ while True:
         led.value = False
     else:
         print('Not logging data. Flip the switch and then hit reset')
-    time.sleep(1) #sleep for so many seconds between measurements
+        if FILEOPEN == True:
+            #This means the file used to be open and we just closed it so we wil properly close the file
+            print('FILE CLOSED!!!')
+            file.close()
+            FILEOPEN = False
+    time.sleep(0.2) #sleep for so many seconds between measurements
