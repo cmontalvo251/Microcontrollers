49f5622f1d58353b759e3d479e552cfb93890b9a
Carlos Montalvo <15804814+cmontalvo251@users.noreply.github.com>
2026-04-17 18:43:23 -0500

    Refactor thermistor code with custom module
    Updated the thermistor code to use a custom module and corrected resistance calculations.

--- Circuit_Playground/CircuitPython/Temp/record_temperature_thermistor.py
+++ Circuit_Playground/CircuitPython/Temp/record_temperature_thermistor.py
@@ -1,25 +1,50 @@
+#This code will test all sorts of different thermistor code
 import time
 import board
-#import adafruit_thermistor
+import adafruit_thermistor
+import montalvo_thermistor #this is a custom module that is basically the adafruit_thermistor but slightly modified
 import analogio
 import math
-#Temperature Sensor is also analog but there is a better way to do it since voltage to temperature
-#Is nonlinear and depends on series resistors and b_coefficient (some heat transfer values)
-pin = analogio.AnalogIn(board.A9) ##If you want analog
+
+##If we're using the adafruit_thermistor module. All we need to do is kick off the module below
 #thermistor = adafruit_thermistor.Thermistor(board.A9, 10000, 10000, 25, 3950)
+#In this case the nominal resistance is 10kO
+#the series resistor is 10kO
+#the nominal temperature is 25.0C
+#and the beta coefficienct is 3950.
+
+##The thermistor module can be downloaded from github but you can also write your own. I've taken the adafruit_thermistor module
+#and created my own with similar parameters and usage
+#Note you can't use both at the same time
+#monte_thermistor = montalvo_thermistor.Thermistor(board.A9,10000,10000,25,3950)
+
+#If you don't want to use a module and you want to directly see where the analog pin value is
+#converted from Digital Output to voltage and then to resistance you can just read the raw analog 
+#signal. To do that use the analogio module
+pin = analogio.AnalogIn(board.A9) ##If you want analog
 
 while True:
-    R = 10000
+    ##Using the thermistor module, we do is just run this block of code
+    #Tmodule = thermistor.temperature
+    #print((Tmodule,))
+    #Or this one if we're using my custom made software
+    #Tmonte = monte_thermistor.temperature
+    #print((Tmonte,))
+    #Or this one if you're reading the raw analog signal
     Do = pin.value
-    dR = R/(2**16/Do-1)
-    #dR = (2**16)/Do*(R) - R #<-It might be this. Double check your math
+    #From here you can easily convert the Digital output to voltage
+    Vth = 3.3*Do/2**16
+    #You can then use the voltage divider rule to solve for the thermistor resistance
+    #noting that this thermistor is connected to the 3.3V side again where the series resistor is 10000
+    Rth = 10000*(3.3/Vth-1)
+    ##And finally you can compute the temperature in celsius
     B = 3950
     To = 25 + 273.1
-    invT = (1/To) + math.log(dR/10000)/B
+    invT = (1/To) + math.log(Rth/10000)/B
     T = 1/invT - 273.1
-    #temp = thermistor.value #if you want analog
-    print((Do,T)) #Remember this command prints Digital Output and Temperature
-    #but you probably want to uncomment the line below
-    #print((time.monotonic(),T))
-    
+    ##Here is where I print everything
+    #First column is time, second column is digital output, third column is voltage across the thermistor, 4th column is resistance
+    #and last column is the temperature in Celsius
+    #You're welcome for getting all of this to work
+    print((time.monotonic(),Do,Vth,Rth,T))
     time.sleep(0.5)
