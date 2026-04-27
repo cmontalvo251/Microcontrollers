6ea5ae0a3dc27ad248ce53a367a7b24a53ad5c8e
Carlos Montalvo Green CrossHair <cmontalvo@southalabama.edu>
2026-04-27 07:02:19 -0500

    Edited read serial cpb for my youtube video

--- Circuit_Playground/CircuitPython/Serial_Handshake/read_serial_cpb.py
+++ Circuit_Playground/CircuitPython/Serial_Handshake/read_serial_cpb.py
@@ -1,9 +1,10 @@
-######################THIS IS UNTESTED CODE!!!!!!
+######################
 
 
 import serial
 import sys
 import time
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 class CPX:
@@ -16,6 +17,7 @@ class CPX:
         self.serialPort = serial.Serial(portname,115200)
         self.t = -99
         self.button = -99
+        self.x = -99
         print('if no errors port opened')
         print('portname: ',portname)
     ##############################################################################
@@ -30,18 +32,19 @@ class CPX:
         inBuffer = self.serialPort.readline()
         instr = str(inBuffer)
         buffer_list = instr.split(' ')
-        #print("buffer:",buffer_list)
+        print("buffer:",buffer_list)
         try:
             self.t = self.convert(buffer_list[0])
             self.button = self.convert(buffer_list[1])
-            outstring = str(self.t) + " " + str(self.button) + "\n"
+            self.x = self.convert(buffer_list[2])
+            outstring = str(self.t) + " " + str(self.button) + " " + str(self.x) + "\n"
             self.file.write(outstring)
             self.file.flush()
         except:
             pass
         
     def printdata(self,time):
-        print(time,self.t,self.button)
+        print(time,self.t,self.button,self.x)
 
     def extractdata(self):
         ###EDIT THIS TO EXTRACT ALL DATA STREAMS #### DONE
@@ -61,6 +64,9 @@ class CPX:
     def close(self):
         self.serialPort.close()
         self.file.close()
+        #If using Thonny in a temp folder you need this extra copy to 
+        #port everything over to a useable directory
+        #os.system('cp CPX0.txt /home/carlos/CP0.txt')
 
 ##############################################################################
 ##############################################################################
